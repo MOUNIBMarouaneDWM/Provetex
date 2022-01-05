5c65885664b3901d597211565ffa0b944f57feb0
MOUNIB Marouane <84908887+MOUNIBMarouaneDWM@users.noreply.github.com>
2022-01-05 13:13:27 -0800

    add method refresh into item

--- Provetex/Item/F_RD.cs
+++ Provetex/Item/F_RD.cs
@@ -19,16 +19,31 @@ namespace Provetex.Item
 
         private void Button_add_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void F_RD_Load(object sender, EventArgs e)
         {
-
+            Refresh();
         }
 
         private void DataGrid_list_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+
+        }
+#pragma warning disable CS0114 // Member hides inherited member; missing override keyword
+        private void Refresh()
+#pragma warning restore CS0114 // Member hides inherited member; missing override keyword
+        {
+
+            DataGrid_list.DataSource = Program.provetex.suppliers_items.Select(item => new
+            {
+                ID = item.C_id_suppliers_items,
+                Article = item.item.C_name_item,
+                Fournisseur = item.supplier.C_name_supplier,
+                Prix = item.C_price
+            }).ToList();
+            DataGrid_list.Columns["ID"].Visible = false;
 
         }
     }
