f3f0966c36fe23f50bcd5d2cd3e288bed18a684b
MOUNIB Marouane <84908887+MOUNIBMarouaneDWM@users.noreply.github.com>
2022-01-27 13:03:26 -0800

    i dont know

--- Provetex/F_loading.Designer.cs
+++ Provetex/F_loading.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // F_loading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "F_loading";
             this.Text = "F_loading";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
